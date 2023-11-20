20f20af788feacb9badde6b71f3a5ffb6573d303
erickt13 <erickantoniotorres@gmail.com>
2023-11-19 18:18:43 -0800

    dotenv installed again

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const expressLayouts = require('express-ejs-layouts')
 const indexRouter = require('./routes/index')
 const PORT = process.env.PORT || 3000
 app.set('view engine', 'ejs')
-app.set('views', __dirname + '/views') 
+app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout') 
 app.use(expressLayouts)
 app.use(express.static('public'))
