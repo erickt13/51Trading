cf8014ef792ad75bf347f2862e5cef6536913d1e
erickt13 <erickantoniotorres@gmail.com>
2023-11-21 18:43:50 -0800

    commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-// if (process.env.NODE_ENV !== 'production') {
-//     require('dotenv').config();
-// }
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 // require('dotenv').config();
 
 // const express = require('express')
@@ -41,7 +41,7 @@
 //     })
 // })
 
-require('dotenv').config();
+// require('dotenv').config();
 const express = require('express')
 const mongoose = require('mongoose')
 
